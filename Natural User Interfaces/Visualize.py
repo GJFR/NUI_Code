2a490250507aa0905fa729e218ded0d14301ed0e
Gertjan Franken <gertjan.franken@gmail.com>
2013-11-08 11:47:29 +0100

    Threshold testen Stijgingen,dalingen herkennen (basis)

--- Natural User Interfaces/Visualize.py
+++ Natural User Interfaces/Visualize.py
@@ -1,7 +1,7 @@
 '''
 Created on 22-okt.-2013
 
-@author: Gertjan
+@author: Gertjan & Kevin
 '''
 
 import csv
@@ -12,6 +12,7 @@ import threading
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+import Threshold
 from scipy.signal import filtfilt, butter
 from time import sleep
 from struct import *
@@ -22,16 +23,16 @@ global eog1_filt
 global eog2_filt
 
 
-with open('C:\\Users\\Gertjan\\git\\NUI_Code\\Data\\test1_A.csv') as csvfile:
+with open('C:\\Users\\Gertjan\\git\\NUI_Code\\Data\\test10_A.csv') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
-        eog1 = [int(i) for i in row]
+        eog1 = [float(i) for i in row]
         print(eog1)
         
-with open('C:\\Users\\Gertjan\\git\\NUI_Code\\Data\\test1_B.csv') as csvfile:
+with open('C:\\Users\\Gertjan\\git\\NUI_Code\\Data\\test10_B.csv') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
-        eog2 = [int(i) for i in row]
+        eog2 = [float(i) for i in row]
         print(eog2)
 
 print(eog1)
@@ -43,12 +44,29 @@ b, a = butter(2, 0.5, 'low')
 eog1_filt = filtfilt(b, a, eog1)
 eog2_filt = filtfilt(b, a, eog2)
 
+def normalize(dataset):
+    mean = sum(dataset)/len(dataset)
+    dataset = dataset - mean
+    dataset = dataset/abs(dataset).max()
+    return dataset
+
+eog1_n = normalize(eog1_filt)
+eog2_n = normalize(eog2_filt)
+
 def plot_data():
     fig = plt.figure()
     ax1 = fig.add_subplot(211)
     ax2 = fig.add_subplot(212)
-    line1, = ax1.plot(eog1_filt)
-    line2, = ax2.plot(eog1)
+    line1, = ax1.plot(eog1_n)
+    line2, = ax2.plot(eog2_n)
     plt.show()
     
 plot_data()
+
+Threshold.setThresholdDifference('left', -0.6)
+Threshold.setThresholdDifference('right', 0.6)
+Threshold.setThresholdDifference('down', 15000)
+Threshold.setThresholdDifference('up', -7000)
+
+
+Threshold.processData(eog2_n)
