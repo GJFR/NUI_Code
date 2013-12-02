6e84641d9963736dd54a0a6752c862e46c9e5177
BKevin <kevin_boets@hotmail.com>
2013-12-02 11:08:38 +0100

    small change

--- Testers/TestSax.py
+++ Testers/TestSax.py
@@ -3,19 +3,21 @@ Created on 2 dec. 2013
 
 @author: Kevin
 '''
-import unittest
 
+import unittest
+import Sax
 
 class TestSax(unittest.TestCase):
+    
+    def setUp(self):
+        self.timeSeq = Sax.TimeSequence(range(10),[0,5,9],2,4,3,4,2,1)
 
-
-    def testName(self):
+    def testConstructor(self):
         pass
 
-
+#####################################################################
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
     
     
-#####################################################################
