85a94183701cb9e1b682fa6bce36813025c7fa72
BKevin <kevin_boets@hotmail.com>
2013-12-09 12:40:20 +0100

    Sax:makeMatchPair(cMatrix) bereken alle distances tussen alle mogelijke paren, Run en Visualize aangepast aan huidige methodes

--- Natural User Interfaces/Visualize.py
+++ Natural User Interfaces/Visualize.py
@@ -66,7 +66,15 @@ def plot_data3(eog1, eog2, diction1, diction2):
         for sequence in diction2[motif]:
             ax2.plot(range(sequence.getStart(), sequence.getStart() + sequence.getLength()), sequence.getOriginal().getAllPoints(), color = 'r')
         plt.show()
-
+        
+def plot_data4(eog1, eog2, motif, matches):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    line1, = ax1.plot(eog2.getMatrix())
+    ax1.plot(range(motif.getStart(), motif.getStart() + motif.getLength()), motif.getOriginal().getAllPoints(), color = 'g')
+    for sequence in matches:
+        ax1.plot(range(sequence.getStart(), sequence.getStart() + sequence.getLength()), sequence.getOriginal().getAllPoints(), color = 'r')
+    plt.show()
 '''
 Threshold.setThresholdDifference('left', -0.6)
 Threshold.setThresholdDifference('right', 0.6)
